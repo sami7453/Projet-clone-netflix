48757803af6b7baff5f2a06909e1b35d93f75a9e
AlexandreImperator <66020831+AlexandreImperator@users.noreply.github.com>
2025-02-19 17:57:44 +0100

    Added missing requires

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,10 @@
-require('./routes/movieRoutes');
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+app.use('/movies', require('./routes/movieRoutes'));
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
