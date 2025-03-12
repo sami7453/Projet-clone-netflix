48ae134b8c78a935ef717293934aae81c1bf8dd6
AlexandreImperator <66020831+AlexandreImperator@users.noreply.github.com>
2025-03-12 16:22:17 +0100

    Added header component

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,6 +1,16 @@
 <template>
-    <router-link to="/login">Login</router-link>
-    <router-view></router-view>
+    <div>
+        <Header />
+        <main>
+            <router-view />
+        </main>
+    </div>
 </template>
+
 <script setup lang="ts">
+    import Header from "./components/HeaderComponent.vue";
 </script>
+
+<style lang="scss">
+
+</style>
---
+++ src/frontend/src/components/HeaderComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+        <router-link to="/">Home</router-link>
+        <router-link to="/search">Search</router-link>
+    </header>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+</style>
